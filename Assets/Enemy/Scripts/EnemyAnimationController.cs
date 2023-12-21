b8f1268bd6ca29a139146c2d4dd2fa8eaee6eb10
Ali Unal <0033484@gmail.com>
2023-12-22 00:22:28 +0300

    Particles added

--- Assets/Enemy/Scripts/EnemyAnimationController.cs
+++ Assets/Enemy/Scripts/EnemyAnimationController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace ArcherAttack.Enemy
@@ -5,8 +6,20 @@ namespace ArcherAttack.Enemy
     public class EnemyAnimationController : MonoBehaviour
     {
         [SerializeField] private Animator _animator;
+
+        [SerializeField] private GameObject _muzzleFlash;
+        public GameObject MuzzleFlash => _muzzleFlash;
+
         public void DisableAnimator() => _animator.enabled = false;
 
         public void Attack() => _animator.SetTrigger("Attack");
+
+        public void ShowMuzzleFlash()
+        {
+            Sequence muzzleFlashSequence = DOTween.Sequence();
+            muzzleFlashSequence.AppendCallback(() => _muzzleFlash.SetActive(true));
+            muzzleFlashSequence.AppendInterval(.02f);
+            muzzleFlashSequence.AppendCallback(() => _muzzleFlash.SetActive(false));
+        }
     }
 }
